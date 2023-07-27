75e19320a7eed36600cb80a8e4c68c99c7f00521
Kamil Wiśniewski <programista171@gmail.com>
2023-07-27 08:45:25 +0200

    tjtjtjtjt

---
+++ src/SwagCustomField.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace SwagCustomField;
+
+use Shopware\Core\Framework\Plugin;
+
+class SwagCustomField extends Plugin
+{
+    public function activate(ActivateContext $context): void
+    {
+        parent::activate($context);
+    }
+}
